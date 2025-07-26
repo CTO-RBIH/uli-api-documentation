f8c54c5fa69e504620809b621b640a024e09bf87
Irfan Shah <irfns@protonmail.com>
2025-07-26 18:01:58 +0530

    init docusaurus for rbih

---
+++ docusaurus.config.ts
@@ -0,0 +1,251 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import type * as Plugin from "@docusaurus/types/src/plugin";
+import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
+
+const config: Config = {
+  title: "RBIH Docs",
+  tagline: "API Documentation for RBIH",
+  url: "https://rbih-docs.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "rbih", // Usually your GitHub org/user name.
+  projectName: "rbih-docs", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.ts"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/rbih/rbih-docs/tree/main/",
+          docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig:
+    {
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+      navbar: {
+        title: "RBIH Docs",
+        logo: {
+          alt: "RBIH Docs Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Documentation",
+          },
+          {
+            label: "RBIH API",
+            position: "left",
+            to: "/docs/category/petstore-api",
+          },
+          {
+            href: "https://github.com/rbih/rbih-docs",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Documentation",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/rbih/rbih-docs",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} RBIH. Built with Docusaurus.`,
+      },
+      prism: {
+        additionalLanguages: [
+          "ruby",
+          "csharp",
+          "php",
+          "java",
+          "powershell",
+          "json",
+          "bash",
+          "dart",
+          "objectivec",
+          "r",
+        ],
+      },
+      languageTabs: [
+        {
+          highlight: "python",
+          language: "python",
+          logoClass: "python",
+        },
+        {
+          highlight: "bash",
+          language: "curl",
+          logoClass: "curl",
+        },
+        {
+          highlight: "csharp",
+          language: "csharp",
+          logoClass: "csharp",
+        },
+        {
+          highlight: "go",
+          language: "go",
+          logoClass: "go",
+        },
+        {
+          highlight: "javascript",
+          language: "nodejs",
+          logoClass: "nodejs",
+        },
+        {
+          highlight: "ruby",
+          language: "ruby",
+          logoClass: "ruby",
+        },
+        {
+          highlight: "php",
+          language: "php",
+          logoClass: "php",
+        },
+        {
+          highlight: "java",
+          language: "java",
+          logoClass: "java",
+          variant: "unirest",
+        },
+        {
+          highlight: "powershell",
+          language: "powershell",
+          logoClass: "powershell",
+        },
+        {
+          highlight: "dart",
+          language: "dart",
+          logoClass: "dart",
+        },
+        {
+          highlight: "javascript",
+          language: "javascript",
+          logoClass: "javascript",
+        },
+        {
+          highlight: "c",
+          language: "c",
+          logoClass: "c",
+        },
+        {
+          highlight: "objective-c",
+          language: "objective-c",
+          logoClass: "objective-c",
+        },
+        {
+          highlight: "ocaml",
+          language: "ocaml",
+          logoClass: "ocaml",
+        },
+        {
+          highlight: "r",
+          language: "r",
+          logoClass: "r",
+        },
+        {
+          highlight: "swift",
+          language: "swift",
+          logoClass: "swift",
+        },
+        {
+          highlight: "kotlin",
+          language: "kotlin",
+          logoClass: "kotlin",
+        },
+        {
+          highlight: "rust",
+          language: "rust",
+          logoClass: "rust",
+        },
+      ],
+    } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          petstore: {
+            specPath: "examples/petstore.yaml",
+            outputDir: "docs/petstore",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag",
+            },
+          } satisfies OpenApiPlugin.Options,
+        } satisfies Plugin.PluginOptions,
+      },
+    ],
+  ],
+
+  themes: ["docusaurus-theme-openapi-docs"],
+};
+
+export default async function createConfig() {
+  return config;
+}
